c784a891f24f177f180d704048b8581324bf3729
tongrhj <tongrhj@gmail.com>
2016-06-23 15:10:21 +0800

    Use PhantomJS only for TravisCI

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,12 @@
 // Karma configuration
+'use strict'
 
 module.exports = function (config) {
+  let customBrowsers = ['Chrome', 'Safari', 'Firefox', 'PhantomJS']
+  if (process.env.TRAVIS) {
+    customBrowsers = ['PhantomJS']
+  }
+
   config.set({
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
@@ -79,7 +85,7 @@ module.exports = function (config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome', 'Safari', 'Firefox', 'PhantomJS'],
+    browsers: customBrowsers,
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
